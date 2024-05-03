92afd3a4eda6edbf74ebb39a2da6ff4fda878825
SargisVardanian <105112007+SargisVardanian@users.noreply.github.com>
2024-05-03 19:35:22 +0400

    Add files via upload

--- C#Script_tester.py
+++ C#Script_tester.py
@@ -13,6 +13,8 @@ from collections import deque, namedtuple
 from torch.distributions import Categorical
 
 
+def count_trainable_params(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 def load_data(file_path):
     try:
@@ -59,6 +61,7 @@ try:
     loss_crit = load_data('/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/data/loss_crit.pkl')
     sum_rewards = load_data('/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/data/sum_rewards.pkl')
     # recent_hid.append(torch.zeros(1, 1, 256))
+
 except:
     recent_actions = deque(maxlen=20)
     recent_states = deque(maxlen=20)
@@ -85,8 +88,8 @@ epsilon = 0.1
 try:
     actor = Actor(input_dim, action_dim)
     critic = Critic(input_dim, action_dim)
-    # actor_critic = Actor_Critic(input_dim, action_dim)
-    # actor_critic.load_state_dict(torch.load('/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/Actor_Critic6_att.pt'))
+    # ddqn = DDQN(input_dim, action_dim)
+    # ddqn.load_state_dict(torch.load('/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/ddqn7.pt'))
     actor.load_state_dict(torch.load('/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/Actor7.pt'))
     critic.load_state_dict(torch.load('/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/Critic7.pt'))
     tf = True
@@ -132,7 +135,7 @@ def generate_action_number(states, state_dict, b_vec, hid=None):
                                 2,
                                 np.where(np.abs(velocity_x)+np.abs(velocity_z) > 7,
                                 10,
-                                np.random.choice([1, 2], size=bbox_w.shape, p=[0.6, 0.4])))),  # Рандомный выбор поворота
+                                np.random.choice([1, 2], size=bbox_w.shape, p=[0.5, 0.5])))),  # Рандомный выбор поворота
                         np.where((bbox_x < 38) & (bbox_x > 0),
                             8,  # Если bbox_x меньше 48 и больше 0, выбираем действие 2
                             np.where((bbox_x > 218) & (bbox_x > 0),
@@ -167,8 +170,8 @@ def generate_action_number(states, state_dict, b_vec, hid=None):
         [-0.1, -0, -0, 0.1, 1.5, 0.5, 0.],  #4
         [-0.0, -0.1, -0.1, 0.2, 0.0, 0.9, 0.],  #5
         [0.2, 0.2, 0.2, 0.1, 0.1, 0.2, 1],  #6
-        [0, 1.2, -0.5, 1., -0.1, 0, 0.7],  #1_7
-        [0, -0.5, 1.2, 1., -0.1, 0, 0.7],  #2_8
+        [0, 1.2, -0.5, .5, -0.1, 0, 0.9],  #1_7
+        [0, -0.5, 1.2, .5, -0.1, 0, 0.9],  #2_8
         [-0.2, 0., 0, 0.7, 0.1, -0.2, 1.5],  #6_9
         [0.2, 0.8, 0.8, -0.1, 0.8, 0.2, 0.2]  #4_10
     ])
@@ -181,7 +184,7 @@ def generate_action_number(states, state_dict, b_vec, hid=None):
     criterion = torch.nn.MSELoss(reduction='none')  # Используем reduction='none' для получения вектора потерь
 
     losses = criterion(probs1, target_vector * 3)
-    # optimizer = torch.optim.AdamW(actor.parameters(), lr=0.000001)
+    # optimizer = torch.optim.AdamW(actor.parameters(), lr=0.00005)
     losses = losses.mean(dim=1)
     # optimizer.zero_grad()
     # for loss in losses:
@@ -191,7 +194,7 @@ def generate_action_number(states, state_dict, b_vec, hid=None):
     actions = m.sample()
     #
     # actions = probs.argmax(1)
-    return actions, probs, m.log_prob(actions), m.entropy(), hid, losses.mean(), actions_targ
+    return actions, probs, m.log_prob(actions), m.entropy(), hid, losses.mean()
 
 def select_action_ddqn(states, b_vec, policy_net, rewards, device='cpu'):
     batch_size = states.size(0)  # Размер батча
@@ -241,7 +244,7 @@ def bb_vectors(state, original_size=256, new_size=128):
     return state_without_bb, bb_vector
 
 
-def send_data(new_states, keys, recent_actions, b_boxes, recent_rewards, recent_hid):
+def send_data(new_states, keys, recent_actions, b_boxes, recent_rewards, recent_hid, alpha_bb=0.25):
     state = torch.from_numpy(new_states).float()
     state_dict = dict(zip(keys, new_states))
     c = ['DisTerr', 'Roll', 'Pitch', 'Yaw', 'RotX', 'RotY', 'RotZ', 'angularVelocityX',  'angularVelocityY', 'angularVelocityZ',  'Bounding_box_X', 'Bounding_box_Y', 'Bounding_box_W', 'Bounding_box_H', 'time', 'Acceleration_x', 'Acceleration_y', 'Acceleration_z', 'AngularAcceleratio_x', 'AngularAcceleratio_y', 'AngularAcceleratio_z']
@@ -260,7 +263,7 @@ def send_data(new_states, keys, recent_actions, b_boxes, recent_rewards, recent_
     state[17] /= 4
     state[20] *= 4
 
-    do = (state[14] < 1.4).int()
+    do = (state[14] < 0.7).int()
     state_t = torch.transpose(state, 0, 1)
     state_only, bb_vec = bb_vectors(state_t)
 
@@ -274,25 +277,27 @@ def send_data(new_states, keys, recent_actions, b_boxes, recent_rewards, recent_
 
 
     b_boxes.append(bb_vec)
+    b_box = list(b_boxes)
+    b = b_box[-1] - alpha_bb * b_box[-2] - alpha_bb**2 * b_box[-3] - alpha_bb**3 * b_box[-4]
 
     rec_act = torch.stack(list(recent_actions), dim=-1)[:, -1].unsqueeze(-1)
-    rec_b_b = torch.stack(list(b_boxes), dim=-1)[:, :, -1]
+    # rec_b_b = torch.stack(b, dim=-1)[:, :, -1]
+    rec_b_b = torch.from_numpy(np.array(b))
     rec_states = torch.stack(list(recent_states), dim=-1)[:, :, -1]
     rec_states = torch.cat([rec_states, rec_act], dim=-1)
-    #
-    #
-    actions_new, new_probs, log_prob, entropy, hidden_state_w, loss, actions_targ = generate_action_number(rec_states, state_dict, rec_b_b)
+
+
+    actions_new, new_probs, log_prob, entropy, hidden_state_w, loss = generate_action_number(rec_states, state_dict, rec_b_b)
     # actions_new = recent_actions[-1]
     # log_prob = recent_log_probs[-1]
     # entropy = entropys[-1]
     # new_probs = recent_probs[-1]
     print(actions_new.tolist(), '\n')
-    print('actions_targ: ', actions_targ)
-
-
+    # print('actions_targ: ', actions_targ)
+    # print('vel_reward: ', vel_reward[5])
+    # print('angular_velocity_reward: ', angular_velocity_reward[5])
     time = (state[14]*2.5).int()
-
-    print('state[14]: ', time)
+    print('time: ', time)
     print('bb_vec: ', torch.sum(bb_vec, dim=-1).long())
     print('state_only: ', state_only[5])
     print('loss: ', loss)
@@ -308,7 +313,15 @@ def send_data(new_states, keys, recent_actions, b_boxes, recent_rewards, recent_
     return recent_states, b_boxes, recent_rewards, actions_new, do,  new_probs, log_prob, entropy, recent_hid, time, loss
 
 
-gamma = 0.99
+gamma = 0.9
+def discount_rewards(reward_tensors, gamma=0.9):
+    rewards = torch.stack(reward_tensors)
+    discount = torch.tensor([gamma**i for i in range(rewards.size(0))]).view(-1, 1)
+    discount = torch.flip(discount, [0])
+    discounted_rewards = rewards * discount
+    discounted_rewards = discounted_rewards.sum(0)
+    discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-8)
+    return discounted_rewards
 
 def train(keys, b_boxes, recent_rewards, recent_hid):
     file_path = sys.argv[1]
@@ -333,22 +346,34 @@ def train(keys, b_boxes, recent_rewards, recent_hid):
     critic_loss_mean = []
     actor.optimizer.zero_grad()  # Обнуляем градиенты актора перед аккумулированием
     critic.optimizer.zero_grad()  # Обнуляем градиенты критика перед аккумулированием
-    for i in range(15, 17):
+    alpha_bb = 0.25
+    b_box = list(b_boxes)
+    for i in range(13, 14):
         rewards = recent_rewards[i]
+        # rewards_next = list(recent_rewards)[i+1:i+3]
+        # print(discount_rewards(torch.tensor(np.array(rewards_next), dtype=torch.float32)))
+        rewards_next = recent_rewards[i+1]
+        rewards_next_ = recent_rewards[i+2]
+        rewards_next__ = recent_rewards[i+3]
 
         done_i = done[i]
         log_prob = recent_log_probs[i-1]
+        entropy = entropys[i-1]
         # probs = recent_probs[i-1]
         actions =recent_actions[i-1].unsqueeze(-1)
 
         act = rec_act[:, i-1].unsqueeze(-1)
-        boxes = rec_b_boxes[:, :, i]
+        b = b_box[i] - alpha_bb * b_box[i-1] - alpha_bb**2 * b_box[i-2] - alpha_bb**3 * b_box[i-3]
+        boxes = torch.from_numpy(np.array(b))
+        # boxes = rec_b_boxes[:, :, i]
         states = rec_state[:, :, i]
         states = torch.cat([states, act], dim=-1)
 
 
         new_act = rec_act[:, i].unsqueeze(-1)
-        boxes_new = rec_b_boxes[:, :, i+1]
+        b = b_box[i+1] - alpha_bb * b_box[i] - alpha_bb**2 * b_box[i-1] - alpha_bb**3 * b_box[i-2]
+        boxes_new = torch.from_numpy(np.array(b))
+        # boxes_new = rec_b_boxes[:, :, i+1]
         states_new = rec_state[:, :, i+1]
         states_new = torch.cat([states_new, new_act], dim=-1)
 
@@ -357,10 +382,10 @@ def train(keys, b_boxes, recent_rewards, recent_hid):
         with torch.no_grad():
             next_values = critic(states_new, boxes_new)
             next_values = next_values.squeeze()
-        advantages = (rewards + (1 - done_i) * gamma * next_values - values).detach()
-        actor_loss = (-log_prob * advantages)
+        advantages = (rewards + (1 - done_i) * (gamma * rewards_next + gamma**2*rewards_next_ + gamma**3*rewards_next__) - values).detach()
+        actor_loss = (-log_prob * advantages) - 0.001 * entropy
 
-        critic_loss = F.huber_loss(values, rewards + (1 - done_i) * gamma * next_values.detach(), reduction='none')
+        critic_loss = F.mse_loss(values, rewards + (1 - done_i) * (gamma * rewards_next + gamma**2*rewards_next_ + gamma**3*rewards_next__).detach(), reduction='none')
 
         actor_loss_mean.append(actor_loss)
         critic_loss_mean.append(critic_loss)
@@ -368,6 +393,12 @@ def train(keys, b_boxes, recent_rewards, recent_hid):
     critic_loss_mean = torch.stack(critic_loss_mean, dim=-1).mean(dim=-1)
     print('actor_loss: ', actor_loss_mean.sum())
     print('critic_loss: ', critic_loss_mean.sum())
+    # total_params_actor = sum(p.numel() for p in actor.parameters() if p.requires_grad)
+    # total_params_critic = sum(p.numel() for p in critic.parameters() if p.requires_grad)
+    #
+    # print("Total trainable parameters in Actor:", total_params_actor)
+    # print("Total trainable parameters in Critic:", total_params_critic)
+
     actor.optimizer.zero_grad()
     critic.optimizer.zero_grad()
     for i in range(12):
@@ -384,16 +415,24 @@ def train(keys, b_boxes, recent_rewards, recent_hid):
     print('advantages: ', advantages)
     print('\n', next_values, '\n', next_actions[5].tolist(), new_probs[5])
     print('prob: ', new_probs, int(time[0]) % 80)
+    # total_params_actor = count_trainable_params(actor)
+    #
+    # ffn_params = set(p for p in critic.ffn.parameters() or critic.x_bb.parameters())
+    # total_params_critic = sum(p.numel() for p in critic.parameters() if p.requires_grad and p not in ffn_params)
+    #
+    # print("Total trainable parameters in Actor:", total_params_actor)
+    # print("Total trainable parameters in Critic:", total_params_critic)
 
-    if int(time[0]) % 80 == 0 & len(loss_act) < 990:
+    if int(time[0]) % 80 == 0:
         loss_act.append(actor_loss_mean.sum())
         loss_crit.append(critic_loss_mean.sum())
-        sum_rewards.append(torch.mean(torch.stack(list(recent_rewards), dim=-1)[-10:]))
+        sum_rewards.append(torch.mean(torch.stack(list(recent_rewards), dim=-1)[-1]))
         loss_.append(loss)
         print('100: save')
     if tf:
         torch.save(actor.state_dict(), '/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/Actor7.pt')
         torch.save(critic.state_dict(), '/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/Critic7.pt')
+        # torch.save(ddqn.state_dict(), '/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/ddqn7.pt')
         print('save')
     save_data(recent_actions, '/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/data/recent_actions.pkl')
     save_data(recent_states, '/Users/sargisvardanyan/PycharmProjects/Quadcopter_RL_car_detection/data/recent_states.pkl')
